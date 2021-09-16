379097458319d15d851c55cd675317dc252d4ce4
DanialGhiaseddin <danial.ghiaseddin@gmail.com>
2021-09-16 12:44:31 +0430

    natural_labels

--- egs/esc50/run_esc.sh
+++ egs/esc50/run_esc.sh
@@ -53,7 +53,7 @@ do
 
   CUDA_CACHE_DISABLE=1 python -W ignore ../../src/run.py --model ${model} --dataset ${dataset} \
   --data-train ${tr_data} --data-val ${te_data} --exp-dir $exp_dir \
-  --label-csv ./data/esc_class_labels_indices.csv --n_class 30 \
+  --label-csv ./data/esc_nat_class_labels_indices.csv --n_class 30 \
   --lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
   --freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
   --tstride $tstride --fstride $fstride --imagenet_pretrain $imagenetpretrain --audioset_pretrain $audiosetpretrain
