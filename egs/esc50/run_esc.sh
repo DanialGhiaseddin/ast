748305b9db1726eb84ed0ec491524b562cb46eec
DanialGhiaseddin <danial.ghiaseddin@gmail.com>
2021-09-16 12:26:49 +0430

    script update

--- egs/esc50/run_esc.sh
+++ egs/esc50/run_esc.sh
@@ -10,8 +10,8 @@
 
 set -x
 # comment this line if not running on sls cluster
-. /data/sls/scratch/share-201907/slstoolchainrc
-source ../../venvast/bin/activate
+#. /data/sls/scratch/share-201907/slstoolchainrc
+#source ../../venvast/bin/activate
 export TORCH_HOME=../../pretrained_models
 
 model=ast
@@ -29,7 +29,7 @@ freqm=24
 timem=96
 mixup=0
 epoch=25
-batch_size=48
+batch_size=12
 fstride=10
 tstride=10
 base_exp_dir=./exp/test-${dataset}-f$fstride-t$tstride-imp$imagenetpretrain-asp$audiosetpretrain-b$batch_size-lr${lr}
@@ -53,7 +53,7 @@ do
 
   CUDA_CACHE_DISABLE=1 python -W ignore ../../src/run.py --model ${model} --dataset ${dataset} \
   --data-train ${tr_data} --data-val ${te_data} --exp-dir $exp_dir \
-  --label-csv ./data/esc_class_labels_indices.csv --n_class 50 \
+  --label-csv ./data/esc_class_labels_indices.csv --n_class 30 \
   --lr $lr --n-epochs ${epoch} --batch-size $batch_size --save_model False \
   --freqm $freqm --timem $timem --mixup ${mixup} --bal ${bal} \
   --tstride $tstride --fstride $fstride --imagenet_pretrain $imagenetpretrain --audioset_pretrain $audiosetpretrain
